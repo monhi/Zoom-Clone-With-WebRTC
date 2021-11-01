24bb09528555b85dc5356eddff1a9eb142279d17
Monhi <m.monhi@gmail.com>
2021-11-01 09:38:52 +0330

    Update server.js
    Following changes are done:
    1- making server a https one instead of http. In this way it is possible to work from different computers. in http mode, browsers can not access to webcams.
    2- change socket.to(roomId).broadcast to socket.broadcast.to(roomId)

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const   fs 			  = require('fs');
+const   path 		  = require('path');
+const   express   = require('express')
+const   https  	  = require('https');
+const   app       = express()
+
+const options = {
+    key:  fs.readFileSync(path.join(__dirname,'ssl','key.pem'), 'utf-8'),
+    cert: fs.readFileSync(path.join(__dirname,'ssl','cert.pem'), 'utf-8')
+}
+const httpsServer 	= https.createServer(options,app);
+
+//const server = require('http').Server(app)
+
+const io = require('socket.io')(httpsServer)
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
@@ -18,12 +29,14 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    //socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.broadcast.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      //socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.broadcast.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
 
-server.listen(3000)
+httpsServer.listen(3000)
